f9450a465e3cb7504c319c2e2d308106f78eee58
sebastianpacurar <sebastianpacurar@users.noreply.github.com>
2020-05-20 01:27:55 +0300

    Following updates were made: (#3)
    - updated the fetch api method to call for different dates regarding currency values
      - updated JSX to support conditional rendering:
        - 1 for the latest exchange rates
        - 1 for exchange rates based on other dates
      - improved data handling for the JSON

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Select from 'react-select';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 
-const Interval = () => {
+// TODO: need to fix this
+const Interval = ({handleCheckBox, checkStatus}) => {
     return (
         <div>
-            <input type='checkbox' id='interval' name='interval' style={{margin: '10px 0 10px 0'}}/>
+            <input
+                type='checkbox'
+                id='interval'
+                name='interval'
+                checked={handleCheckBox}
+                style={{margin: '10px 0 10px 0'}}
+            />
             <label htmlFor='interval' style={{margin: '10px 0 10px 0'}}> Interval</label>
         </div>
     );
 }
 
 
-const StartInput = ({maxDate, performOnChange}) => {
+const MaxDateInput = ({maxDate, performOnChange}) => {
 
     return (
         <div>
@@ -29,7 +36,7 @@ const StartInput = ({maxDate, performOnChange}) => {
 }
 
 
-const StopInput = ({maxDate, performOnChange}) => {
+const MinDateInput = ({maxDate, performOnChange}) => {
 
     return (
         <div>
@@ -45,49 +52,8 @@ const StopInput = ({maxDate, performOnChange}) => {
 }
 
 
-const DisplayButton = ({performOnClick}) => {
-
-    return <button onClick={performOnClick} className='form'>Display results</button>
-}
-
-
 // the Select component used for creating the Select option functionality
 const SelectCurrency = ({performOnChange}) => {
-
-    // options are composed of 2 properties: values and labels
-    const options = [
-        {value: 'AUD', label: 'Australian Dollar'},
-        {value: 'BGN', label: 'Bulgarian Lev'},
-        {value: 'BRL', label: 'Brazilian Real'},
-        {value: 'CAD', label: 'Canadian Dollar'},
-        {value: 'CNY', label: 'Chinese Yuan'},
-        {value: 'HRK', label: 'Croatian Kuna'},
-        {value: 'CZK', label: 'Czech Koruna'},
-        {value: 'DKK', label: 'Danish Krone'},
-        {value: 'GBP', label: 'Great Britain Pound Sterling'},
-        {value: 'HKD', label: 'Hong Kong Dollar'},
-        {value: 'HUF', label: 'Hungarian Forint'},
-        {value: 'INR', label: 'Indian Rupee'},
-        {value: 'IDR', label: 'Indonesian Rupiah'},
-        {value: 'ILS', label: 'Israeli New Shekel'},
-        {value: 'JPY', label: 'Japanese Yen'},
-        {value: 'MYR', label: 'Malaysian Ringgit'},
-        {value: 'MXN', label: 'Mexican Peso'},
-        {value: 'NZD', label: 'New Zealand Dollar'},
-        {value: 'NOK', label: 'Norwegian Krone'},
-        {value: 'PSP', label: 'Philippine Peso'},
-        {value: 'PLN', label: 'Poland Zloty'},
-        {value: 'RON', label: 'Romanian Leu'},
-        {value: 'RUB', label: 'Russian Ruble'},
-        {value: 'SGD', label: 'Singapore Dollar'},
-        {value: 'ZAR', label: 'South African Rand'},
-        {value: 'KRW', label: 'South Korean Won'},
-        {value: 'SEK', label: 'Swedish Krona'},
-        {value: 'CHF', label: 'Swiss Franc'},
-        {value: 'THB', label: 'Thai Baht'},
-        {value: 'TRY', label: 'Turkish lira'},
-        {value: 'USD', label: 'United States Dollar'},
-    ]
 
     return (
         <Select options={options} placeholder='EUR' onChange={performOnChange}/>
@@ -101,74 +67,179 @@ const App = () => {
     const [date] = useState(new Date().toISOString().split("T")[0]);
 
 
-    // the input and output states refer to the input of type date used to grab currency in different time spans
-    const [inputVal, setInputVal] = useState(date);
-    const [outputVal, setOutputVal] = useState(date);
+    // the input and output states refer to the input of type date used to grab currency in different time spans. defaults to current date
+    const [minInputVal, setMinInputVal] = useState(date);
+    const [maxOutputVal, setMaxOutputVal] = useState(date);
 
-    // TODO: need to implement
+    // used for querying rates from other dates mentioned in the max and min input
     const [checked, setChecked] = useState(false);
-
 
     // the state used to grab information from the SelectCurrency component
     const [selected, setSelected] = useState('EUR');
 
-
     // the states used for grabbing the API data
-    const [data, setData] = useState({});
-
+    const [data, setData] = useState([]);
 
     // handle the input start date value
-    const handleInputChange = (e) => setInputVal(e);
-
+    const handleInputChange = e => setMinInputVal(e);
 
     // handle the input end date value
-    const handleOutputChange = (e) => setOutputVal(e);
+    const handleOutputChange = e => setMaxOutputVal(e);
 
     // handle the Select option
-    const handleOptionChange = (option) => setSelected(option);
+    const handleOptionChange = option => setSelected(option);
+
+    //TODO: need to fix this
+    // handle the Interval checkbox for multiple dates
+    const handleIntervalCheck = () => setChecked(!checked);
 
 
-    // fetch the API for the given details
-    const handleClick = async () => {
+    // fetch data from the API for the given details
+    const fetchData = async () => {
 
-        let url = `https://api.exchangeratesapi.io/latest?base=${selected}`;
+        let result = []
+
+        // let url = `https://api.exchangeratesapi.io/latest?base=${selected}`;
+        // let url = `https://api.exchangeratesapi.io/history?start_at=${minInputVal}&end_at=${maxOutputVal}&base=${selected}`;
+
+        //Todo for test purposes
+        let url = `https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2018-01-05&base=${selected}`;
 
         if (!checked) {
 
             const response = await fetch(url);
             const jsonData = await response.json();
-            setData(jsonData.rates);
+
+            const items = jsonData.rates;
+
+            // parse the data into an array of object elements which will also contain the date property for every single currency property
+            for (let date in items) {
+
+                if (items.hasOwnProperty(date)) {
+                    for (let curr in items[date]) {
+
+                        if (items[date].hasOwnProperty(curr)) {
+                            result.push({'currency': curr, 'value': items[date][curr], 'date': date});
+                        }
+                    }
+                }
+            }
+
+            // sort the array from lowest day to last day
+            result.sort((a, b) => {
+                const firstDate = new Date(a.date),
+                    secondDate = new Date(b.date);
+
+                return firstDate - secondDate;
+            })
+
+
+        } else {
+            const response = await fetch(url);
+            const jsonData = await response.json();
+            const currencies = jsonData.rates;
+
+            for (let curr in currencies) {
+
+                // parse the data into an array of object elements
+                if (currencies.hasOwnProperty(curr)) {
+                    result.push({"currency": curr, "value": currencies[curr]});
+                }
+            }
         }
+        return result;
+
     };
 
+    // in case the result contains different dates for the currencies, it executes second after Layout Effect
+    useEffect(() => {
+        fetchData().then(res => setData(res));
+    }, [selected]);
+
+
     return (
-        <div>
-            <StartInput maxDate={date} performOnChange={(e) => handleInputChange(e.target.value)}/>
-            <StopInput maxDate={date} performOnChange={(e) => handleOutputChange(e.target.value)}/>
-            <Interval/>
+        <Fragment>
+            <MaxDateInput maxDate={date} performOnChange={(e) => handleInputChange(e.target.value)}/>
+            <MinDateInput maxDate={date} performOnChange={(e) => handleOutputChange(e.target.value)}/>
+            <Interval
+                checkStatus={checked}
+                performOnChange={handleIntervalCheck}
+            />
 
             {/*in order to set the state of the Select, it will be performed using onChange and the target is the option.value*/}
             <SelectCurrency performOnChange={(option) => handleOptionChange(option.value)}/>
-            <DisplayButton performOnClick={handleClick}/>
 
-            <Fragment>
+            <table>
+
                 <thead>
                 <tr>
-                    <th>Currency</th>
-                    <th>Value in {selected}</th>
+                    <th>Currency Code</th>
+                    <th>Currency Value in {selected}</th>
+                    <th>Date</th>
                 </tr>
                 </thead>
 
-                {/*map the json into an array composed of arrays (every array contains first element as key and second as value*/}
-                {Object.entries(data).map(([key, value], index) =>
-                    <tr key={index}>
-                        <td>{key}</td>
-                        <td>{value}</td>
-                    </tr>)}
-            </Fragment>
-        </div>
+                {checked ?  // if checked is true
+
+                    //map the json into an array composed of objects (every object contains currency, value and date)
+                    data.map((item, index) =>
+                        <tr key={index}>
+                            <td>{Object.values(item)[0]}</td>
+                            <td>{Object.values(item)[1]}</td>
+                            <td>{Object.values(item)[2]}</td>
+                        </tr>)
+
+
+                    :   // if checked is false
+
+                    //map the json into an array composed of objects (every object contains currency and value)
+                    data.map((item, index) =>
+                        <tr key={index}>
+                            <td>{Object.values(item)[0]}</td>
+                            <td>{Object.values(item)[1]}</td>
+                            <td>{date}</td>
+                        </tr>)
+                }
+            </table>
+        </Fragment>
     );
 }
+
+
+// options are composed of 2 properties: values and labels
+const options = [
+    {value: 'AUD', label: 'Australian Dollar'},
+    {value: 'BGN', label: 'Bulgarian Lev'},
+    {value: 'BRL', label: 'Brazilian Real'},
+    {value: 'CAD', label: 'Canadian Dollar'},
+    {value: 'CNY', label: 'Chinese Yuan'},
+    {value: 'HRK', label: 'Croatian Kuna'},
+    {value: 'CZK', label: 'Czech Koruna'},
+    {value: 'DKK', label: 'Danish Krone'},
+    {value: 'GBP', label: 'Great Britain Pound Sterling'},
+    {value: 'HKD', label: 'Hong Kong Dollar'},
+    {value: 'HUF', label: 'Hungarian Forint'},
+    {value: 'INR', label: 'Indian Rupee'},
+    {value: 'IDR', label: 'Indonesian Rupiah'},
+    {value: 'ILS', label: 'Israeli New Shekel'},
+    {value: 'JPY', label: 'Japanese Yen'},
+    {value: 'MYR', label: 'Malaysian Ringgit'},
+    {value: 'MXN', label: 'Mexican Peso'},
+    {value: 'NZD', label: 'New Zealand Dollar'},
+    {value: 'NOK', label: 'Norwegian Krone'},
+    {value: 'PSP', label: 'Philippine Peso'},
+    {value: 'PLN', label: 'Poland Zloty'},
+    {value: 'RON', label: 'Romanian Leu'},
+    {value: 'RUB', label: 'Russian Ruble'},
+    {value: 'SGD', label: 'Singapore Dollar'},
+    {value: 'ZAR', label: 'South African Rand'},
+    {value: 'KRW', label: 'South Korean Won'},
+    {value: 'SEK', label: 'Swedish Krona'},
+    {value: 'CHF', label: 'Swiss Franc'},
+    {value: 'THB', label: 'Thai Baht'},
+    {value: 'TRY', label: 'Turkish lira'},
+    {value: 'USD', label: 'United States Dollar'},
+]
 
 
 ReactDOM.render(
